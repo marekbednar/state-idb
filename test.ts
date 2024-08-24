3586d84afc30518f698fd2be19982806ea6b4372
marek.bednar <marek.bednar@nordlicht-it.com>
2024-08-24 20:25:27 +0200

    - working example added

--- test.ts
+++ test.ts
@@ -75,3 +75,68 @@ export async function main() {
 
 }
 
+// working code example
+// this.initIdb();
+// 				let toLoad = 3;
+// 				const decreasesLoadedAndCheck = () => {
+// 					toLoad--;
+// 					if (toLoad === 0) {
+// 						this.getStore().dataPreloaded.setState(true);
+// 					}
+// 				}
+//
+// 				getRecordById("settings", "members").then((res) => {
+// 					console.log('members last updated', res)
+// 					if (res === undefined || res + this.storeTimeout < new Date().getMilliseconds()) {
+// 						RhmMemberController.searchAll().pipe(
+// 							catchError(e => of([]))
+// 						).subscribe((members) => {
+// 							clearState('members');
+// 							setState(members, 'members');
+// 							setRecordById("settings", "members", {updated: Date.now()});
+// 							decreasesLoadedAndCheck();
+// 						});
+// 					} else {
+// 						decreasesLoadedAndCheck();
+// 					}
+// 				});
+// 				getRecordById("settings", "rhmcases").then((res) => {
+// 					console.log('rhmcases last updated', res)
+// 					if (res === undefined || res + this.storeTimeout < new Date().getMilliseconds()) {
+// 						RhmRecordController.search(filteredUserResponse.company.id).pipe(
+// 							catchError(e => of([]))
+// 						).subscribe((records) => {
+// 							clearState('rhmcases');
+// 							setState(records, 'rhmcases');
+// 							setRecordById("settings", "rhmcases", {updated: Date.now()});
+// 							decreasesLoadedAndCheck();
+// 						});
+// 					} else {
+// 						decreasesLoadedAndCheck();
+// 					}
+// 				});
+// 				getRecordById("settings", "notifications").then((res) => {
+// 					console.log('notifications last updated', res)
+// 					NotificationController.search(0, new Date().getTime()).pipe(catchError(e => of([]))).subscribe((notifications) => {
+// 						if (res === undefined || res + this.storeTimeout < new Date().getMilliseconds()) {
+// 							clearState('notifications');
+// 							setState(notifications, 'notifications');
+// 							setRecordById("settings", "notifications", {updated: Date.now()});
+// 							decreasesLoadedAndCheck();
+// 						} else {
+// 							decreasesLoadedAndCheck();
+// 						}
+// 					});
+// 				});
+// 				this.getStore().dataPreloaded.setState(true);
+//
+//initIdb() {
+// 		initializeDatabase([
+// 			{ store: 'members', indexes: [] },
+// 			{ store: 'rhmcases', indexes: [] },
+// 			{ store: 'notifications', indexes: [] },
+// 			{ store: 'settings', indexes: [], settings: {
+// 					autoIncrement: false
+// 				} }
+// 		]);
+// 	}
